6acb48ede61233028ad68a442e4b32791f9ce19e
kwfredeen <kwfredeen@gmail.com>
2018-01-19 18:50:20 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(291, 110);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Count: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(288, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Double Click: 0";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.DTapClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Next Upgrade: 100";
+            this.label2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(520, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SaveClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(288, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Auto Click: 0";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.AClick_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Next Upgrade: 1000";
+            this.label3.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 341);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "ClickerGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        string[] savelines = { "CLICKS = 0", "DTAP = 0", "DTAPU = 50", "ACLICK = 0", "ACLICKU = 1000", "ASPEED = 0" };
+
+        Int64 clicks = 0;
+
+        int DTap = 0;
+        int DTapUpCost = 50; //Change as balance requires
+        int DTapUpInit = 50;
+        int DTapUpMult = 2; //Change as balance requires
+
+        int AClick = 0;
+        int AClickUpCost = 1000; //Change as balance requires
+        int AclickUpInit = 1000;
+        int AClickUpMult = 10; //Change as balance requires
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Int64 processLine(int lineNum)
+        {
+            string line = savelines[lineNum];
+            int beginPos = line.LastIndexOf(' ');
+
+            return Convert.ToInt64(line.Substring(beginPos)) ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //debug console
+            //AllocConsole();
+            Console.WriteLine("Console initialized.");
+
+            //Check if a savefile exists
+            if (!System.IO.File.Exists(@"..\save.txt"))
+            {
+                //if not, create one
+                System.IO.File.WriteAllLines(@"..\save.txt", savelines);
+                Console.WriteLine("Created new save file.");
+            }
+
+            savelines = System.IO.File.ReadAllLines(@"..\save.txt");
+
+            //load clicks
+            clicks = processLine(0);
+
+            //load DTap
+            DTap = (int) processLine(1);
+            DTapUpCost = (int) processLine(2);
+
+            //Load AClick
+            AClick = (int) processLine(3);
+            AClickUpCost = (int) processLine(4);
+
+            Console.WriteLine("Loaded from save file");
+            
+            //Begin AClick proces
+            backgroundWorker1.RunWorkerAsync();
+
+            //Render text objects
+            label1.Text = "Count: " + clicks.ToString();
+
+            button2.Text = "Double Click: " + DTap.ToString();
+            label2.Text = "Next Upgrade: " + DTapUpCost.ToString();
+            if (DTap > 0 | clicks > DTapUpInit)
+            {
+                button2.Visible = true;
+                label2.Visible = true;
+            }
+
+            button4.Text = "Auto Click: " + AClick.ToString();
+            label3.Text = "Next Upgrade: " + AClickUpCost.ToString();
+            if (AClick > 0 | clicks > AclickUpInit)
+            {
+                button4.Visible = true;
+                label3.Visible = true;
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e) //Click button
+        {
+            clicks  = clicks + (int) Math.Pow(2, DTap);
+
+            if (clicks == 666)
+            {
+                label1.Text = "Count: s8n";
+            }
+            if (clicks >= DTapUpInit)
+            {
+                button2.Visible = true;
+                label2.Visible = true;
+            }
+            if (clicks >= AclickUpInit)
+            {
+                button4.Visible = true;
+                label3.Visible = true;
+            }
+
+            label1.Text = "Count: " + clicks.ToString();
+        }
+
+        private void DTapClick(object sender, EventArgs e) //Double Click Button
+        {
+            //Dtap stuff here
+            if (clicks < DTapUpCost)
+            {
+                //Maybe display a message in the future using a blank label next to it?
+                return;
+            }
+            clicks = clicks - DTapUpCost;
+            DTapUpCost = DTapUpCost * DTapUpMult; //change with balance
+            DTap++;
+
+            label1.Text = "Count: " + clicks.ToString();
+            button2.Text = "Double Click: " + DTap.ToString();
+            label2.Text = "Next Upgrade: " + DTapUpCost.ToString();
+        }
+
+        private void SaveClick(object sender, EventArgs e)
+        {
+            savelines[0] = "CLICKS = " + clicks.ToString();
+            savelines[1] = "DTAP = " + DTap.ToString();
+            savelines[2] = "DTAPU = " + DTapUpCost.ToString();
+            savelines[3] = "ACLICK = " + AClick.ToString();
+            savelines[4] = "ACLICKU = " + AClickUpCost.ToString();
+
+            System.IO.File.WriteAllLines(@"..\save.txt", savelines);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AClick_Click(object sender, EventArgs e)
+        {
+            if (clicks < AClickUpCost)
+            {
+                //Maybe display a message in the future using a blank label next to it?
+                return;
+            }
+
+            clicks = clicks - AClickUpCost;
+            AClickUpCost = AClickUpCost * AClickUpMult;
+            AClick++;
+
+            label1.Text = "Count: " + clicks.ToString();
+            button4.Text = "Auto Click: " + AClick.ToString();
+            label3.Text = "Next Uprade: " + AClickUpCost.ToString();
+        }
+            
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                if (AClick == 0)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    clicks = clicks + (int)Math.Pow(2, AClick - 1);
+                }
+                worker.ReportProgress(0);
+            }
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = "Count: " + clicks.ToString();
+        }
+    }
+}
